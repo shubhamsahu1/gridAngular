13e599c42a7435e2fb0a04f688d464b6db6904bf
shubhamsahu16 <shubhamsahu.sahu@gmail.com>
2020-04-20 13:47:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'grid';
+  grid:boolean=true
+  gridCount=Array(21)
+  toggele(){
+    this.grid = !this.grid
+  }
 }
